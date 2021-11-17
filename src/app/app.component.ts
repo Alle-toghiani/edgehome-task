d2f5591ad232c0ea3ad380ca65cbf414fec0d944
Ali Toghiani <ali.toghiani.dev@gmail.com>
2021-11-17 18:58:54 +0330

    feat(app.component): first draft, generate a 200x100 json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as faker from 'faker';
+import { address } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'edgehome';
+  columnDefs = []
+  rowData = []
+  dataset = [];
+ categories = []
+
+  ngOnInit(){
+  this.categories = [];
+  Object.keys(faker).forEach(element => {
+    this.categories.push({title: element, data: Object.keys(faker[element])});
+  })
+  this.categories = this.categories.filter( element =>
+    ( element.title !=='commerce')
+  )
+  this.categories[14].data = this.categories[14].data.slice(0,3);
+  // let rowsCount = 0;
+let exitLoop = false;
+  loop1:
+for(let rowsCount = 0 ; rowsCount<199;){
+        let columnsCount = 0;
+        let row = {};
+        exitLoop = false;
+        loop2: 
+          for ( let categoryIndex = 10 ; categoryIndex< this.categories.length; categoryIndex++){
+            loop3:
+              for (let subCat = 0; subCat< this.categories[categoryIndex].data.length; subCat++){
+              let temp1 = faker.fake(`{{${this.categories[categoryIndex].title}.${this.categories[categoryIndex].data[subCat]}}}`);
+              
+              if (this.categories[categoryIndex].title !== 'imageUrl'){
+                row[this.categories[categoryIndex].data[subCat]] = temp1.substring(0, 10);
+              } else {
+                row[this.categories[categoryIndex].data[subCat]] = temp1;
+              }
+              // this.dataset.push(`{{${this.categories[categoryIndex].title}.${this.categories[categoryIndex].data[subCat]}}}`);
+              columnsCount++;
+              if(columnsCount == 99){
+                this.dataset.push(row);
+                rowsCount++;
+                row = {};
+                let categoryIndex = 10
+                exitLoop = true;
+                break;
+              }
+              }
+              if (exitLoop)
+              break;
+          }
+      }
+    console.log(faker.fake(`{{image.imageUrl}}`));
+    this.rowData= this.dataset;
+    Object.keys(this.dataset[0]).forEach(header => {
+      this.columnDefs.push({headerName: header, field: header});
+    })
+  }
 }
+
